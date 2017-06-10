4c998d8c320e0d21df542c216a32573161c2fefa
James Pike <jpike@chilon.net>
2017-06-10 21:13:36 +0100

    implementation and test

---
+++ src/index.spec.ts
@@ -0,0 +1,24 @@
+import 'mocha'
+import { Observable } from 'rxjs/Observable'
+import { TestScheduler } from 'rxjs/testing/TestScheduler'
+import 'rxjs/add/observable/of'
+import { expect, assert } from 'chai'
+
+import RateLimiter from '.'
+
+describe('rxjs-ratelimiter', () => {
+  it('queues events according to rate limit', () => {
+    const scheduler = new TestScheduler(assert.deepEqual)
+    const limiter = new RateLimiter(2, 10, scheduler)
+
+    let nObserved = 0
+    const limitObservable = value => limiter.limit(Observable.of(value))
+
+    scheduler.expectObservable(limitObservable('a')).toBe('(a|)')
+    scheduler.expectObservable(limitObservable('b')).toBe('(b|)')
+    scheduler.expectObservable(limitObservable('c')).toBe('-(c|)')
+    scheduler.expectObservable(limitObservable('d')).toBe('-(d|)')
+    scheduler.expectObservable(limitObservable('e')).toBe('--(e|)')
+    scheduler.flush()
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
 import { Observable } from 'rxjs/Observable'
+import { Scheduler } from 'rxjs/Scheduler'
+import { async as asyncScheduler } from 'rxjs/scheduler/async'
+import 'rxjs/add/observable/of'
+import 'rxjs/add/operator/concatMap'
+import 'rxjs/add/operator/switchMapTo'
+import 'rxjs/add/operator/delay'
 
 export default class RateLimiter {
   private intervalEnds = 0
+  private nActiveInCurrentInterval = 0
 
-  constructor(private requestsPerInterval: number, private intervalLength: number) {}
+  constructor(
+    private requestsPerInterval: number,
+    private intervalLength: number,
+    private scheduler: Scheduler = asyncScheduler,
+  ) {}
 
   limit<T>(stream: Observable<T>): Observable<T> {
-    // TODO: rate limiting
-    return stream
+    return Observable.of(null).concatMap(() => {
+      const now = this.scheduler.now()
+      if (this.intervalEnds <= now) {
+        this.nActiveInCurrentInterval = 1
+        this.intervalEnds = now + this.intervalLength
+        return stream
+      } else {
+        if (++this.nActiveInCurrentInterval > this.requestsPerInterval) {
+          this.nActiveInCurrentInterval = 1
+          this.intervalEnds += this.intervalLength
+        }
+
+        const wait = this.intervalEnds - this.intervalLength - now
+        return wait > 0 ? Observable.of(null).delay(wait, this.scheduler).switchMapTo(stream) : stream
+      }
+    })
   }
 }
