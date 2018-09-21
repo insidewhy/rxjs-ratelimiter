6a1f2e6e393929301ba6e9f541468cd8dd05fb00
Joel Aufgang <joel.aufgang@gmail.com>
2018-09-21 16:06:35 -0400

    upgraded to rxjs 6

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import { Observable } from 'rxjs/Observable'
-import { Scheduler } from 'rxjs/Scheduler'
-import { async as asyncScheduler } from 'rxjs/scheduler/async'
-import 'rxjs/add/observable/of'
-import 'rxjs/add/operator/concatMap'
-import 'rxjs/add/operator/switchMapTo'
-import 'rxjs/add/operator/delay'
+import {asyncScheduler, Observable, of, Scheduler} from "rxjs";
+import {concatMap, delay, switchMapTo} from "rxjs/operators";
 
 export default class RateLimiter {
   private intervalEnds = 0
@@ -17,21 +12,28 @@ export default class RateLimiter {
   ) {}
 
   limit<T>(stream: Observable<T>): Observable<T> {
-    return Observable.of(null).concatMap(() => {
-      const now = this.scheduler.now()
-      if (this.intervalEnds <= now) {
-        this.nActiveInCurrentInterval = 1
-        this.intervalEnds = now + this.intervalLength
-        return stream
-      } else {
-        if (++this.nActiveInCurrentInterval > this.requestsPerInterval) {
+    return of(null).pipe(
+      concatMap(() => {
+        const now = this.scheduler.now()
+        if (this.intervalEnds <= now) {
           this.nActiveInCurrentInterval = 1
-          this.intervalEnds += this.intervalLength
-        }
+          this.intervalEnds = now + this.intervalLength
+          return stream
+        } else {
+          if (++this.nActiveInCurrentInterval > this.requestsPerInterval) {
+            this.nActiveInCurrentInterval = 1
+            this.intervalEnds += this.intervalLength
+          }
 
-        const wait = this.intervalEnds - this.intervalLength - now
-        return wait > 0 ? Observable.of(null).delay(wait, this.scheduler).switchMapTo(stream) : stream
-      }
-    })
+          const wait = this.intervalEnds - this.intervalLength - now
+          return wait > 0
+            ? of(null).pipe(
+              delay(wait, this.scheduler),
+              switchMapTo(stream)
+            )
+            : stream
+        }
+      })
+    )
   }
 }
